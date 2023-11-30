b49cd7e6fc85aa665c4dee10bb435f29fc91a724
Melo-Edvaldo <professor.edvaldomelo@gmail.com>
2023-11-30 11:29:52 -0300

    Criando variaveis.php e git.txt

---
+++ variaveis.php
@@ -0,0 +1,9 @@
+<?php
+# Variáveis
+/*
+Os nomes de variáveis sempre começam com o caractere $ (cifrão).
+São sensíveis à letra, ou seja, $valor é diferente de $Valor.
+Um nome de variável válido deve iniciar com uma letra ou sublinhado.
+Nunca deve ser iniciado com um número ou caractere especial.
+*/
+?>
